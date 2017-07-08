8ff42f1d5b87311374d4e632068539c224cef7e7
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-07-08 13:47:07 -0400

    removed patterns module, added packages to install for others

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from apps.users import views
 from django.contrib.auth.decorators import login_required
-urlpatterns = patterns('',
+
+urlpatterns = [
 	url(r'^$', views.Register.as_view(), name='users-register'),
 	url(r'^login/$', views.Login.as_view(), name='users-login'),
 	url(r'^logout/$', views.Logout.as_view(), name='users-logout'),
-	)
+	]
