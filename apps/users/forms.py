63faf476bc875c4f4473171a17a6edda214cda01
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-07-14 19:01:41 -0400

    modified forms

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-from models import UserProfile
+# from models import UserProfile
 
 class UserProfileForm(forms.ModelForm):
-	
+
 
 	class Meta:
 		model = UserProfile
