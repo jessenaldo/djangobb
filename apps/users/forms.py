1bc0b8cb1c0487b92a85a47508d0f50d2896a145
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-07-14 20:07:25 -0400

    edited model in forms

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-# from models import UserProfile
+from users.models import UserProfile
 
 class UserProfileForm(forms.ModelForm):
 
